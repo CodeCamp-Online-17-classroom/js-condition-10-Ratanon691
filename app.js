4347811ba1d6a0875a6c422114f6712ca875e60e
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-22 16:02:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+let user = prompt(`username`)
+if(user == `` || user == null){
+  alert(`username is required`)
+}
+else{
+let password = prompt(`password`)
+if(password == `` || password == null){
+  alert(`password is required`)
+}
+else if (user == `admin` && password == 1234 || user == `John` && password == `qwerty`){
+  alert(`Hello, ${user}`)
+}
+else {
+  alert(`Invalid username or password`)
+}
+}
